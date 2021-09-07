a27d9dba03f6f0a59280b07b26248b4edd32ad81
Termux32-70 <63185453+Anton064@users.noreply.github.com>
2021-09-07 10:35:47 +0300

    upgrade 1.0.2

--- index.py
+++ index.py
@@ -145,7 +145,7 @@ def start():
         version = "1.0.2"
         print("Обновление текущая версия " + str(version))
         extra.write(f"{re}[-]{nu}||||||||||||||||||||{gr}[+]{nu}")
-        os.system("./.lib/.upgrade.sh")
+        os.system("./.upgrade.sh")
     elif function_number == "6":
         os.system("clear")
         print("Завершение роботы файла.")
